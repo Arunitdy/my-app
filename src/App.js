ffe50fadb83227f4515d0973a7e86cd10ea964bb
Arun <arunmundakkal003@gmail.com>
2024-12-11 18:37:44 +0530

    profile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from './login';
 import Chef from './Chef/Chef';
 import Count from "./Count/Count";
-
+import Profile  from "./Profile/Profile";
 function App() {
   return (
     <div className="App">
@@ -13,6 +13,7 @@ function App() {
 
         <Count/>
         */
+        <Profile/>
         }
     </div>
   );
